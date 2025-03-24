4207a63c63849d1f63ce910600c80e49ff48c590
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-03-24 11:24:21 +0100

    Update

--- examples/classification/IMDbSetsTextCategorizationDemo.py
+++ examples/classification/IMDbSetsTextCategorizationDemo.py
@@ -257,7 +257,7 @@ def default_args(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument("--number-of-clauses", default=10, type=int)
     parser.add_argument("--T", default=100, type=int)
-    parser.add_argument("--s", default=1.0, type=float)
+    parser.add_argument("--s", default=1.5, type=float)
     parser.add_argument("--platform", default="CPU_sets", type=str)
     parser.add_argument("--weighted-clauses", default=True, type=bool)
     parser.add_argument("--epochs", default=1000, type=int)
